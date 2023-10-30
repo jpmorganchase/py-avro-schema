ee5b3ab72527505a7e3d36c2b42759213045077a
Tobias Sauerwein <cgtobi@gmail.com>
2023-10-30 11:34:37 +0100

    docs: update types docs

--- src/py_avro_schema/_schemas.py
+++ src/py_avro_schema/_schemas.py
@@ -573,8 +573,11 @@ class UnionSchema(Schema):
     def handles_type(cls, py_type: Type) -> bool:
         """Whether this schema class can represent a given Python class"""
         origin = get_origin(py_type)
+
+        # Support for `X | Y` syntax available in Python 3.10+
+        # equivalent to `typing.Union[X, Y]`
         if getattr(types, "UnionType", None):
-            return origin == Union or origin == types.UnionType
+            return origin == Union or origin == types.UnionType  # noqa: E721
         return origin == Union
 
     def __init__(self, py_type: Type[Union[Any]], namespace: Optional[str] = None, options: Option = Option(0)):
